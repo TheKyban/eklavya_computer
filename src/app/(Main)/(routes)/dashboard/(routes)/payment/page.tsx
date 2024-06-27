82fdbfbddc69adb3e864d75514e7e311cdd3991a
aditya kumar <aaditya1392@gmail.com>
2024-06-27 22:28:10 +0530

    bank details and qr.

--- src/app/(Main)/(routes)/dashboard/(routes)/payment/page.tsx
+++ src/app/(Main)/(routes)/dashboard/(routes)/payment/page.tsx
@@ -1,3 +1,4 @@
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import Image from "next/image";
 import { FC } from "react";
 
@@ -5,7 +6,7 @@ interface pageProps {}
 
 const Payment: FC<pageProps> = ({}) => {
     return (
-        <div className="flex h-[calc(100vh-80px)] items-center justify-center">
+        <div className="flex h-[calc(100vh-80px)] items-center justify-center gap-10 flex-wrap">
             <Image
                 src={"/payment.jpeg"}
                 alt="upi qr code"
@@ -13,6 +14,31 @@ const Payment: FC<pageProps> = ({}) => {
                 height={400}
                 className="rounded-2xl drop-shadow-lg"
             />
+            <div className="flex flex-col gap-2">
+                <h1 className="text-center">Bank Details</h1>
+                <Table className="px-1">
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>Account Number</TableCell>
+                            <TableCell>00000042912910762</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Account Name</TableCell>
+                            <TableCell>
+                                EKLAVAYA UNIVERSAL PRIVATE LIMITED
+                            </TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>BANK NAME</TableCell>
+                            <TableCell>SBI</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>IFSC CODE</TableCell>
+                            <TableCell>SBIN0018042</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
         </div>
     );
 };
