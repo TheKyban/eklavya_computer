248d7beef19a5cd81790c5ad0c28bfd9b6730f9e
aditya kumar <aaditya1392@gmail.com>
2025-02-05 18:04:56 +0530

    now admin can enter and edit marks

--- src/app/api/(public)/branch/route.ts
+++ src/app/api/(public)/branch/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { Prisma } from "../../../../../prisma/prisma";
 
+export const dynamic = "force-dynamic";
 export const GET = async (req: Request) => {
     try {
         const branch = await Prisma.user.findMany({
