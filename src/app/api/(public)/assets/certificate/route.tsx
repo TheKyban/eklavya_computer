a522e1498ae37bc5e043b5be5e4f2035b2cb5103
Aditya Kumar <aaditya1392@gmail.com>
2025-04-14 16:11:44 +0530

    added revalidation in api route for /assets

--- src/app/api/(public)/assets/certificate/route.tsx
+++ src/app/api/(public)/assets/certificate/route.tsx
@@ -6,6 +6,7 @@ import { CertificateTemplate } from "./CERTIFICATE_TEMPLATE";
 import { StudentWithAllDetails } from "@/lib/TYPES";
 import { DOCUMENT_SIZES } from "@/lib/CONSTANTS";
 
+export const revalidate = 0;
 export const dynamic = "force-dynamic";
 
 export const GET = async (req: Request) => {
--- src/app/api/(public)/assets/marksheet/route.tsx
+++ src/app/api/(public)/assets/marksheet/route.tsx
@@ -6,6 +6,7 @@ import { MarksheetTemplate } from "./MARKSHEET_TEMPLATE";
 import { StudentWithAllDetails } from "@/lib/TYPES";
 import { DOCUMENT_SIZES } from "@/lib/CONSTANTS";
 
+export const revalidate = 0;
 export const dynamic = "force-dynamic";
 
 export const GET = async (req: Request) => {
